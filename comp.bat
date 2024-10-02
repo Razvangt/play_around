c67f2c69548e093dd01c9c769fbbabeb0640287c
Razvan <raz@windows.es>
2024-10-02 21:39:11 +0200

    vulkan introduction 0 to vertex buffers

--- comp.bat
+++
@@ -1,5 +0,0 @@
-g++  -c src/function.cpp  -o build/function.o 
-g++  src/main.cpp build/function.o -o build/output.exe 
-
-
-%~dp0/build/output.exe
--- src/function.cpp
+++
@@ -1,5 +0,0 @@
-#include "../include/function.h"
-
-void what_happened(){
-  printf("What happened from the function\n");
-}

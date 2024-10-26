a05d52ac5a56e5e53c16f34147252aff6a4106fc
Razvan <rasvan@hotmail.es>
2024-10-26 15:52:56 +0200

    build improved

--- lua_scripts/build.lua
+++ lua_scripts/build.lua
@@ -1,69 +1,87 @@
 require "lua_scripts.env_variables"
-local flags = " -Wall -MJ fragments -g -std=c++20 "
+
+local flags_list = {}
+local include_list = {}
+local search_lib_list = {}
+local link_lib_list = {}
+local source_list = {}
+local compile_list = {}
+
+local function include_path(path,folder)
+  local result = "-I" .. path .. "/" .. folder
+  table.insert(include_list,result)
+end
+local function lib_path(path,folder)
+  path =  "-L" .. path .. "/" .. folder
+  table.insert(search_lib_list,path)
+end
+
+local function lib_to_link(name)
+  local result =  "-l" .. name
+  table.insert(link_lib_list,result)
+end
+local function add_flags(name)
+  table.insert(flags_list,name)
+end
+local function add_source(name)
+  table.insert(source_list,name)
+end
+local function add_compile(name)
+  table.insert(compile_list,name)
+end
 
 
-local vulkan_include = "-I" .. LibPaths.vulkan .. "/Include"
-local vulkan_lib_dir = "-L" .. LibPaths.vulkan .. "/Lib"
-local vulkan_lib = "-lvulkan-1"
 
-local glfw_include = "-I" .. LibPaths.glfw .. "/include"
-local glfw_lib_dir = "-L" .. LibPaths.glfw .. "/lib"
-local glfw_lib = "-lglfw3dll "
 
-local glm_include = "-I" .. LibPaths.glm .. "/include"
-local glm_lib_dir = "-L" .. LibPaths.glm .. "/lib"
-local glm_lib = "-lglm"
+--- LIBRARIES 
 
--- Targets
-local include = table.concat({
-  vulkan_include,
-  " ",
-  glfw_include,
-  " ",
-  glm_include
-})
-local search_lib = table.concat({
-  vulkan_lib_dir,
-  " ",
-  glfw_lib_dir,
-  " ",
-  glm_lib_dir
-})
-local link_lib = table.concat({
-  vulkan_lib,
-  " ",
-  glfw_lib,
-  " ",
-  glm_lib
-})
+include_path(LibPaths.stb,"include")
+include_path(LibPaths.vulkan, "Include")
+include_path(LibPaths.glfw, "include")
+include_path(LibPaths.glm, "include")
 
-local source = table.concat({
-  " src/main.cpp",
-  " src/engine/debug.cpp",
-  " src/engine/vk_tools.cpp",
-  " src/engine/window.cpp",
-  " src/engine/render_pipeline.cpp",
-  " src/engine/swapchain.cpp",
-  " src/engine/vk_commands.cpp",
-  " src/engine/engine_lib.cpp",
-})
+lib_path(LibPaths.vulkan, "Lib")
+lib_path(LibPaths.glfw, "lib")
+lib_path(LibPaths.glm, "lib")
 
+lib_to_link("vulkan-1")
+lib_to_link("glfw3dll")
+lib_to_link("glm")
+
+-- COMPILER
+local compiler = "clang++"
+add_flags("-Wall")
+add_flags("-MJ fragments ")
+add_flags("-g")
+add_flags("-std=c++20")
+
+--- SOURCE 
+add_source("src/main.cpp")
+add_source("src/engine/debug.cpp")
+add_source("src/engine/vk_tools.cpp")
+add_source("src/engine/window.cpp")
+add_source("src/engine/render_pipeline.cpp")
+add_source("src/engine/swapchain.cpp")
+add_source("src/engine/vk_commands.cpp")
+add_source("src/engine/engine_lib.cpp")
+local result = "-o build/main.exe"
 
 print("----------Compile  program --------------")
-local compile = table.concat({
-  "clang++ ",
-  flags,
-  " ",
-  include,
-  " ",
-  search_lib,
-  " ",
-  link_lib,
-  " ",
-  source,
-  " -o build/main.exe",
-})
+local include = table.concat(include_list," ")
+local search_lib = table.concat(search_lib_list," ")
+local link_lib = table.concat(link_lib_list," ")
+local flags =  table.concat(flags_list, " ")
+local source = table.concat(source_list, " ")
+add_compile(compiler)
+add_compile(flags)
+add_compile(include)
+add_compile(search_lib)
+add_compile(link_lib)
+add_compile(source)
+add_compile(result)
+local compile = table.concat(compile_list," ")
 print(compile)
+
 os.execute(compile)
 
 
--- lua_scripts/set_up.lua
+++ lua_scripts/set_up.lua
@@ -4,11 +4,12 @@ require "lua_scripts.env_variables"
 os.execute("mkdir build")
 
 
+
+-- In windows we need the glwf dll from the bin in the build file
 local origin = LibPaths.glfw .. "/bin"
 local dest = PrPath .. "/build"
 local glfw_command = 'xcopy ' .. origin .. ' ' .. dest
 glfw_command = string.gsub(glfw_command,'/','\\');
 glfw_command = glfw_command .. " /d"
-
 print(glfw_command)
 os.execute(glfw_command )

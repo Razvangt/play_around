0a792cada5aecf480b714229008bb716bc65f568
Razvan <rasvan@hotmail.es>
2024-11-10 16:44:18 +0100

    fix -> LOAD MODEL

--- src/engine/pipeline.cpp
+++ src/engine/pipeline.cpp
@@ -793,18 +793,21 @@ void loadModel(struct Engine_App* state) {
     for (const auto& shape : shapes) {
         for (const auto& index : shape.mesh.indices) {
             Vertex vertex{};
+
             vertex.pos = { attrib.vertices[3 * index.vertex_index + 0], attrib.vertices[3 * index.vertex_index + 1],
                 attrib.vertices[3 * index.vertex_index + 2] };
+
             vertex.texCoord = { attrib.texcoords[2 * index.texcoord_index + 0],
                 1.0f - attrib.texcoords[2 * index.texcoord_index + 1] };
-            vertex.texCoord = { attrib.texcoords[2 * index.texcoord_index + 0], attrib.texcoords[2 * index.texcoord_index + 1] };
 
-            vertex.color = { 1.0f, 1.0f, 1.0f };
+            vertex.color    = { 1.0f, 1.0f, 1.0f };
+
             if (uniqueVertices.count(vertex) == 0) {
-                uniqueVertices[vertex] = static_cast<uint32_t>(state ->vertices.size());
+                uniqueVertices[vertex] = static_cast<uint32_t>(state->vertices.size());
                 state->vertices.push_back(vertex);
             }
-            state->indices.push_back(state->indices.size());
+
+            state->indices.push_back(uniqueVertices[vertex]);
         }
     }
 }
